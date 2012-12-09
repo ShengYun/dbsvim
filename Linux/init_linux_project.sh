f8d62197f2649355b1c60af70706e6fc5268dbf1
Sheng Yun <uewing@126.com>
2012-12-09 14:01:34 +0800

    Reconstructe folder structure
    Adding platform specific files to each
    corresponding folder.

---
+++ Linux/init_linux_project.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/sh
+
+rm tags
+rm cscope.files
+rm cscope.out
+rm *.taghl
+
+find . -iname '*.c' -exec echo "\"{}\"" ';' -o -iname '*.cpp' -exec echo "\"{}\"" ';' -o -iname '*.cc' -exec echo "\"{}\"" ';' -o -iname '*.h' -exec echo "\"{}\"" ';' -o -iname '*.hpp' -exec echo "\"{}\"" ';' -o -iname '*.py' -exec echo "\"{}\"" ';' > cscope.files
+
+cscope -b -i cscope.files -f cscope.out
+
+ctags -R --c++-kinds=+p --fields=+ialS --extra=+q .
