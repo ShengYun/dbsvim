68a4e655211948bb484156e8b156632a6b75d9bc
Sheng Yun <uewing@126.com>
2012-12-20 19:42:01 +0800

    Add java, perl, php to create project script

--- Linux/init_linux_project.sh
+++ Linux/init_linux_project.sh
@@ -10,6 +10,9 @@ find . -iname '*.c' -exec echo "\"{}\"" ';' ^
 -o -iname '*.cc' -exec echo "\"{}\"" ';' ^
 -o -iname '*.h' -exec echo "\"{}\"" ';' ^
 -o -iname '*.hpp' -exec echo "\"{}\"" ';' ^
+-o -iname '*.java' -exec echo "\"{}\"" ';' ^
+-o -iname '*.php' -exec echo "\"{}\"" ';' ^
+-o -iname '*.pl' -exec echo "\"{}\"" ';' ^
 -o -iname '*.py' -exec echo "\"{}\"" ';' > cscope.files
 
 cscope -b -i cscope.files -f cscope.out
